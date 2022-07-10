d29d7abf2161290a8ace922480f12109eee44593
Jeff <jeff.musgrave@gmail.com>
2022-07-10 00:18:31 -0400

    added alt text for github logo

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -7,8 +7,16 @@ export default function Footer() {
     <Section outerPy="5rem" flexDir="column">
       <VStack gap={5}>
         <Aside />
-        <Link target="_blank" href="https://github.com/creamofthecoin/btcsecuritybudget" zIndex="1" w="32px">
-          <Image src="/github-logo.png" />
+        <Link
+          target="_blank"
+          href="https://github.com/creamofthecoin/btcsecuritybudget"
+          zIndex="1"
+          w="32px"
+        >
+          <Image
+            src="/github-logo.png"
+            alt="Link to BTC Security Budget's Github page."
+          />
         </Link>
       </VStack>
     </Section>
