adc406803614262a613c1a18e1efa3046532d4dc
Jeff <jeff.musgrave@gmail.com>
2022-07-12 00:12:16 -0400

    set position fixed and w /h to percent

--- pages/index.js
+++ pages/index.js
@@ -70,10 +70,14 @@ export default function Home() {
         transition={{ delay: 0.5 }}
         style={{
           backgroundColor: "#1A202C",
-          position: "absolute",
-          height: "100vh",
-          width: "100vw",
+          position: "fixed",
+          height: "100%",
+          width: "100%",
           display: "grid",
+          top: "0",
+          bottom: "0",
+          right: "0",
+          left: "0",
           placeItems: "center",
         }}
       >
