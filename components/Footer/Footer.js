4f7b145d7a36e3d36ac5ab2e6a4de852a5d69185
Jeff <jeff.musgrave@gmail.com>
2022-07-12 01:10:51 -0400

    added thought experiment to footer

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,4 +1,4 @@
-import { Image, Link, VStack } from "@chakra-ui/react";
+import { Image, Link, Text, VStack } from "@chakra-ui/react";
 import Aside from "../Aside/Aside";
 import Section from "../Section/Section";
 
@@ -7,11 +7,15 @@ export default function Footer() {
     <Section outerPy="2rem" flexDir="column">
       <VStack gap={5}>
         <Aside />
+        <Text p="0 !important" fontSize="xs" color="gray.100">
+          A thought experiment to encourage discussion.
+        </Text>
         <Link
           target="_blank"
           href="https://github.com/creamofthecoin/btcsecuritybudget"
           zIndex="1"
           w="32px"
+          opacity="0.75"
         >
           <Image
             src="/github-logo.png"
