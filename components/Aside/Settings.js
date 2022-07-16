dbcabc2d23d28a8677a240e6b69dfbf2014e3344
Jeff <jeff.musgrave@gmail.com>
2022-07-16 01:07:36 -0400

    added padding in settings.

--- components/Aside/Settings.js
+++ components/Aside/Settings.js
@@ -14,21 +14,23 @@ export default function Settings({
     <>
       <VStack alignItems="stretch">
         <ModalHeading>Settings</ModalHeading>
-        <InputWithLabel
-          label="Fee Meme Threshold (USD)"
-          value={feeMemeThreshold}
-          onChange={setFeeMemeThreshold}
-        />
-        <InputWithLabel
-          label="Block Size Meme Threshold (MB)"
-          value={blockSizeMemeThreshold}
-          onChange={setBlockSizeMemeThreshold}
-        />
-        <InputWithLabel
-          label="Security Meme Threshold (%)"
-          value={securityMemeThreshold * 100}
-          onChange={(x) => setSecurityMemeThreshold(x / 100)}
-        />
+        <VStack pt="2rem" alignItems="stretch">
+          <InputWithLabel
+            label="Fee Meme Threshold (USD)"
+            value={feeMemeThreshold}
+            onChange={setFeeMemeThreshold}
+          />
+          <InputWithLabel
+            label="Block Size Meme Threshold (MB)"
+            value={blockSizeMemeThreshold}
+            onChange={setBlockSizeMemeThreshold}
+          />
+          <InputWithLabel
+            label="Security Meme Threshold (%)"
+            value={securityMemeThreshold * 100}
+            onChange={(x) => setSecurityMemeThreshold(x / 100)}
+          />
+        </VStack>
       </VStack>
     </>
   );
