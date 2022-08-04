687ae82c65b618ce553f245c0464c1a20c6d48ae
KevinMusgrave <tkm45@cornell.edu>
2022-08-04 09:01:02 -0400

    List of links

--- components/Aside/Solutions.js
+++ components/Aside/Solutions.js
@@ -1,16 +1,40 @@
-import { Text } from "@chakra-ui/react";
+import { Link, ListItem, UnorderedList } from "@chakra-ui/react";
 import _ from "lodash";
 import ModalHeading from "../../utils/ModalHeading";
 
-const drivechain = { heading: "Drivechain", text: "..." };
-const spacechain = { heading: "Spacechain", text: "..." };
-const dynamicBlockSize = { heading: "Dynamic Block Size", text: "..." };
-const solutionList = [drivechain, spacechain, dynamicBlockSize];
+const drivechain = {
+  heading: "Drivechain",
+  links: [
+    { href: "https://www.drivechain.info/", name: "Drivechain Website" },
+    {
+      href: "https://twitter.com/Truthcoin/status/1471487934352216065",
+      name: "Twitter Thread",
+    },
+  ],
+};
+const spacechain = {
+  heading: "Spacechain",
+  links: [
+    {
+      href: "https://www.youtube.com/watch?v=N2ow4Q34Jeg&feature=youtu.be&ab_channel=RubenSomsen",
+      name: "Video Explanation",
+    },
+  ],
+};
+// const dynamicBlockSize = { heading: "Dynamic Block Size", links: ["..."] };
+const solutionList = [drivechain, spacechain];
 
 export default function Solutions() {
   return _.shuffle(solutionList).map((x) => (
     <div key={x.heading}>
-      <ModalHeading>{x.heading}</ModalHeading> <Text>{x.text}</Text>
+      <ModalHeading>{x.heading}</ModalHeading>
+      <UnorderedList>
+        {x.links.map((y) => (
+          <ListItem key={y.href}>
+            <Link href={y.href}>{y.name}</Link>
+          </ListItem>
+        ))}
+      </UnorderedList>
     </div>
   ));
 }
